2d0f1784787a558a2bdee4e41c3139f8b999e413
rasel <russelroy15@gmail.com>
2021-09-18 18:58:41 +0600

    WIP React core cencept two

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Product name="Mobile" price="12000"></Product>
+      <Product name="Mobile" price="12000"></Product>
+      <Product name="Mobile" price="12000"></Product>
+      <Product name="Mobile" price="12000"></Product>
+    </div>
+  );
+}
+
+function Product(props) {
+  return (
+    <div>
+      <h2>Name: {props.name}</h2>
+      <h4>Price: {props.price}</h4>
+    </div>
+  );
 }
 
 export default App;
