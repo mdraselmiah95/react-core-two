6eedbf733cbed5e66c4b392d21ff97fca30992f9
rasel <russelroy15@gmail.com>
2021-09-18 19:32:47 +0600

    WIP React core cencept two

--- src/App.js
+++ src/App.js
@@ -1,14 +1,7 @@
 import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <Product name="Mobile" price="12000"></Product>
-      <Product name="Laptop" price="77000"></Product>
-      <Product name="Watch" price="21000"></Product>
-      <Product name="Camera" price="33000"></Product>
-    </div>
-  );
+  return <div className="App"></div>;
 }
 
 function Product(props) {
@@ -24,4 +17,29 @@ function Product(props) {
   );
 }
 
+/* 
+
+  const products = [
+    { name: "Smart television", price: 122000 },
+    { name: "Apple watch", price: 55000 },
+    { name: "Realme mobile", price: 25000 },
+    { name: "SunGlass", price: 5000 },
+    { name: "Tablet", price: 33000 },
+  ];
+
+*/
+
+/* 
+
+    {products.map((product) => (
+        <Product name={product.name} price={product.price}></Product>
+      ))}
+
+      <Product name="Mobile" price="12000"></Product>
+      <Product name="Laptop" price="77000"></Product>
+      <Product name="Watch" price="21000"></Product>
+      <Product name="Camera" price="33000"></Product>
+
+
+*/
 export default App;
