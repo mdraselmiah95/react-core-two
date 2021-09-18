fb0662f409d7034d1ea15e3350fbc32b20d17e16
rasel <russelroy15@gmail.com>
2021-09-18 19:05:29 +0600

    WIP React core cencept two

--- src/App.js
+++ src/App.js
@@ -4,16 +4,20 @@ function App() {
   return (
     <div className="App">
       <Product name="Mobile" price="12000"></Product>
-      <Product name="Mobile" price="12000"></Product>
-      <Product name="Mobile" price="12000"></Product>
-      <Product name="Mobile" price="12000"></Product>
+      <Product name="Laptop" price="77000"></Product>
+      <Product name="Watch" price="21000"></Product>
+      <Product name="Camera" price="33000"></Product>
     </div>
   );
 }
 
 function Product(props) {
+  const productStyle = {
+    margin: "10px 8px",
+    color: "#eb6e34",
+  };
   return (
-    <div className="product">
+    <div className="product" style={productStyle}>
       <h2>Name: {props.name}</h2>
       <h4>Price: {props.price}</h4>
     </div>
